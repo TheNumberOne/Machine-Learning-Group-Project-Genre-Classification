ea65fc6028f3e0c319ecc287d243b816e040100e
MotlJohn <motljohn@isu.edu>
2019-11-06 11:30:56 -0700

    Added classes for parsing data.

---
+++ data/MachineLearningAttempt1/MachineLearningAttempt1/Data/BookData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachineLearningAttempt1.Data {
+    /// <summary>
+    /// Holds data on a book. Includes word counts, genre, and name.
+    /// </summary>
+    class BookData {
+        private string _genre;
+        private string _name;
+
+        private Dictionary<string, WordData> _words;
+        
+        public Dictionary<string, WordData> Words {
+            get { return _words; }
+        }
+        public int TotalWordCount {
+            get { return Words.Count; }
+        }
+    }
+}
---
+++ data/MachineLearningAttempt1/MachineLearningAttempt1/Data/WordData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachineLearningAttempt1.Data {
+    /// <summary>
+    /// Word count and frequency for a word in a book.
+    /// </summary>
+    public class WordData {
+        private string _word;
+        private int _count;
+        private BookData _bookData;
+
+        public string Word {
+            get { return _word; }
+        }
+        public int Count {
+            get { return _count; }
+        }
+        public double Frequency {
+            get { return _count / _bookData.TotalWordCount; }
+        }
+    }
+}
