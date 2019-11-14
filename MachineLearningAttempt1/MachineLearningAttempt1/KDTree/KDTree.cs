ba2e8ae61db686b1f6711a591f156b6dd272815d
MotlJohn <motljohn@isu.edu>
2019-11-14 09:16:06 -0700

    Added initial class definitions for KDTree.

---
+++ MachineLearningAttempt1/MachineLearningAttempt1/KDTree/KDTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachineLearningAttempt1.KDTree {
+    public class KDTree {
+        protected const double UNKNOWN = double.MaxValue;
+
+        private List<DataType> _columnDataTypes;
+        private InternalNode _rootNode;
+
+
+
+
+        enum DataType {
+            continuous,
+            nominal
+        }
+    }
+}
