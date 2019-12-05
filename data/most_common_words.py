d614303492a452754e2a1d4fb1f06d2ddb77f0bb
Rosetta Roberts <roberose@isu.edu>
2019-12-04 21:07:06 -0700

    fixed typo

--- data/most_common_words.py
+++ data/most_common_words.py
@@ -10,6 +10,6 @@ data = pd.read_csv('results/gutenberg.csv', index_col='gutenberg id')
 words = Counter()
 for gutenberg_id in data.index:
     words.update(extract_words(gutenberg_id))
-	
-most_common_pd = pd.DataFrame(data=most_common, columns=["words", "freq"])
+
+most_common_pd = pd.DataFrame(data=words.most_common(), columns=["words", "freq"])
 most_common_pd.to_csv("results/most-common-words.csv")
